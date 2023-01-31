b08b5ed281099ac33a36ee836c521f673710f315
michelangelo <michelangelocali@hotmail.com>
2023-01-30 22:12:06 -0300

    swagger added and configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 /* eslint-disable indent */
 import express, { Express } from 'express'
+import swaggerUi from 'swagger-ui-express'
 
+import swaggerFile from '../config/swagger.json'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specifications.routes'
 
 export const routes = (app: Express) => {
   app.use(express.json()),
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile)),
     app.use('/categories', categoriesRoutes),
     app.use('/specifications', specificationsRoutes)
 }
